00488d2c7a38683fc7ef390e7b637a94743f3b53
YUYING LU <148526765+YUYING-LU@users.noreply.github.com>
2026-06-27 17:50:11 -0400

    Delete R directory

--- R/MuSiC-master/zzz.R
+++
@@ -1,4 +0,0 @@
-.onAttach <- function(libname, pkgname) {
-  packageStartupMessage("MuSiC v1.0.0 support SingleCellExperiment! See Tutorial: https://xuranw.github.io/MuSiC/articles/MuSiC.html")
-  packageStartupMessage("MuSiC2 for multi-condition bulk RNA-seq data is also available!")
-}
